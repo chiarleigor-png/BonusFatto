5aa371004b6ae872d71a0e86d37603099f15a90c
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-13 21:06:04 +0200

    Enable UI-only home restyle

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './AppV2.jsx';
 import './styles.css';
 import './v2.css';
 import './demo.css';
+import './restyle.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
