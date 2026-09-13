e96a0d679bbd570eb2a92e175b1ecf7519052dfc
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-13 19:35:42 +0200

    Load temporary demo mode styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './AppV2.jsx';
 import './styles.css';
 import './v2.css';
+import './demo.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
