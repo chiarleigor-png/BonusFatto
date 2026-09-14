8c15f434752dd4aee11aeae467691aa298e241f0
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-14 23:33:17 +0200

    Ripristina prezzo report BonusFatto a 9,90 euro

--- api/checkout.js
+++ api/checkout.js
@@ -7,7 +7,7 @@ const PLANS = {
   },
   report: {
     name: 'BonusFatto - Relazione PDF + modello email/PEC TARI',
-    amount: 100,
+    amount: 990,
   },
   whatsapp: {
     name: 'BonusFatto - Bonus Alert WhatsApp 12 mesi',
