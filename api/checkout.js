51d35956727107fc1c95c25230cfaddfbb65c31d
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-14 22:02:57 +0200

    Imposta temporaneamente checkout base a 1 euro per test

--- api/checkout.js
+++ api/checkout.js
@@ -3,7 +3,7 @@ import { randomBytes } from 'node:crypto';
 const PLANS = {
   base: {
     name: 'BonusFatto - Analisi completa',
-    amount: 499,
+    amount: 100,
   },
   report: {
     name: 'BonusFatto - Relazione PDF + modello email/PEC TARI',
