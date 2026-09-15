c55715f4dc84e59014ab1fc7233b812a143bd0e3
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-16 00:05:32 +0200

    Ripristina rendering home rimuovendo entry ISEE esterna

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import './restyle.css';
 import './family-hero.css';
 import './results-v3.css';
 import './paywallV3.js';
-import './iseeEntry.js';
 import './legalSite.js';
 import './guideNav.js';
 import './brandLogo.js';
