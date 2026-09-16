c22c21875a57bf4119d5cb5797a2128c8febb077
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-16 21:47:35 +0200

    Attiva il nuovo flusso ISEE con domande integrative

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './clearLegacyProfile.js';
-import App from './AppV4.jsx';
+import App from './AppV5.jsx';
 import './styles.css';
 import './v2.css';
 import './demo.css';
