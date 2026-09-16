a1f0d6675b8910ae58c6c6995d21739420ddeb1f
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-16 21:18:57 +0200

    Fix result rendering after calculator submit

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,6 @@ import './guideNav.js';
 import './brandLogo.js';
 import './formEnhancements.js';
 import './reportDelivery.js';
-import './mobileResultScroll.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
