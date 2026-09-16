96ec9e2383a2a13bb5d0a492741d1de25d9c36b1
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-16 09:32:10 +0200

    Collega il nuovo funnel ISEE pre-pagamento

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppV3.jsx';
+import App from './AppV4.jsx';
 import './styles.css';
 import './v2.css';
 import './demo.css';
