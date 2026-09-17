ce0dad423adc08b286f3804ed9ebf660f98a12af
chiarleigor-png <chiarle.igor@gmail.com>
2026-09-17 06:32:34 +0200

    Attiva nuovo flusso ISEE corretto

--- src/AppV8.jsx
+++ src/AppV8.jsx
@@ -1,8 +1,8 @@
 import AppV7 from './AppV7.jsx';
-import IseeReportFlowV3 from './IseeReportFlowV3.jsx';
+import IseeReportFlowV4 from './IseeReportFlowV4.jsx';
 
 export default function AppV8() {
   const params = new URLSearchParams(window.location.search);
-  if (params.get('isee_preview') === '1') return <IseeReportFlowV3 />;
+  if (params.get('isee_preview') === '1') return <IseeReportFlowV4 />;
   return <AppV7 />;
 }
